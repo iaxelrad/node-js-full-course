aee1fcda7b7336cc59e0d43329f80b1334173405
Itamar Axelrad <iaxelrad@gmail.com>
2022-05-27 17:41:59 +0300

    exit on uncaught errors

--- Chapter 02 - Read and Write Files/index.js
+++ Chapter 02 - Read and Write Files/index.js
@@ -4,3 +4,9 @@ fs.readFile('./files/starter.txt', 'utf-8', (err, data) => {
   if (err) throw err;
   console.log(data);
 });
+
+//exit on uncaught errors
+process.on('uncaughtException', err => {
+  console.error(`There was an uncaught error: ${err}`);
+  process.exit(1);
+});
