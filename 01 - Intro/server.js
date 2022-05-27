b7267fa30c69c132bd4b1de04870e36397f5d78f
Itamar Axelrad <iaxelrad@gmail.com>
2022-05-27 17:20:13 +0300

    exploring the path module

--- 01 - Intro/server.js
+++ 01 - Intro/server.js
@@ -8,6 +8,7 @@ console.log('Hello World');
 // 5) CommonJS modules instead of ES6 modules
 
 const os = require('os');
+const path = require('path');
 
 console.log(os.type());
 console.log(os.version());
@@ -15,3 +16,9 @@ console.log(os.homedir());
 
 console.log(__dirname);
 console.log(__filename);
+
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+
+console.log(path.parse(__filename));
