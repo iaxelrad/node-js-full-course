83c4db17fd4e97dcdb47384d1e10b80d9c33d7ab
Itamar Axelrad <iaxelrad@gmail.com>
2022-05-27 17:45:32 +0300

    Replaced hardcoded file path with path.join

--- Chapter 02 - Read and Write Files/index.js
+++ Chapter 02 - Read and Write Files/index.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
+const path = require('path');
 
-fs.readFile('./files/starter.txt', 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+fs.readFile(
+  path.join(__dirname, 'files', 'starter.txt'),
+  'utf-8',
+  (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  }
+);
 
 console.log('Hello...');
 
